e01e21a812c96f7839c0a6274c5a9f8d5ba70067
ohsyme <gamingrkp@gmail.com>
2022-06-30 18:11:08 +0700

    Update test.js

--- test/test.js
+++ test/test.js
@@ -1,13 +1,28 @@
-const nanaApi = require("../index");
-let API = new nanaApi();
+const puppeteer = require('puppeteer-extra')
+const cheerio = require('cheerio')
+const StealthPlugin = require('puppeteer-extra-plugin-stealth');
+const { default: axios } = require('axios');
+puppeteer.use(StealthPlugin())
 
-(async function () {
-  console.log("not valid");
-  console.log(await API.g("ShindoL"));
-  console.log("valid");
-  console.log(await API.g("177013"));
-  console.log("not valid");
-  console.log(await API.search("GYVDYVADSVAUDVSYUABDISAVYDASBIAVSYUVDAYUVSDDASVYU"));
-  console.log("valid");
-  console.log(await API.search("metamorphosis"));
-})();
+
+const url = `https://nhentai.net/api/gallery/`
+async function data(){
+  const browser = await puppeteer.launch({headless: false});
+  const page = await browser.newPage();
+  await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.67 Safari/537.36 OPR/87.0.4390.58')
+  await page.goto('https://nhentai.net/');
+  await page.waitForTimeout(5000)
+
+  const pagedata = await page.evaluate(() => {
+    return {
+        html: document.documentElement.innerHTML,
+        width: document.documentElement.clientWidth,
+        heigh: document.documentElement.clientHeight,
+    }
+  })
+
+
+  return pagedata
+}
+
+
